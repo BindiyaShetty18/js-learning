2a3d306eb66094fed293b71781f71d719fe1b400
BindiyaShetty18 <shettybindiya18@gmail.com>
2024-03-09 00:23:59 +0530

    Completed with the basic variable programs and swapping
    Completed with the basic variable programs and swapping

---
+++ 01_swapVariablesAssign.js
@@ -0,0 +1,32 @@
+console.log("----STEP 1----");
+console.log("====Before Swap====");
+var sweety = "Sweety";
+var cutie = "Cutie";
+console.log("Sweety :",sweety);
+console.log("Cutie :",cutie);
+
+console.log("====After Swap====");
+var temp = sweety;
+sweety = cutie;
+cutie = temp;
+console.log("Sweety :",sweety);
+console.log("Cutie :",cutie);
+
+console.log("----STEP 2----");
+console.log("====Before Swap====");
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+console.log("Num 1 :",num1);
+console.log("Num 2 :",num2);
+console.log("Num 3 :",num3);
+
+console.log("====After Swap====");
+var temp2 = 100;
+num1 = num2;
+num2 = num3;
+num3 = temp2;
+console.log("Num 1 :",num1);
+console.log("Num 2 :",num2);
+console.log("Num 3 :",num3);
+
---
+++ 01_variableAssign.js
@@ -0,0 +1,35 @@
+console.log("========Step 1========");
+var name;
+name = undefined;
+console.log("My name is :", name);
+
+console.log("=========After Initialization==========");
+name = "Bindiya Shetty";
+console.log("My Name is :", name);
+
+console.log("=======After Update=========");
+name = "Bindiya Ganesh Shetty";
+console.log("My Full Name Is :", name);
+
+console.log("-------------------------------------");
+var myLove;
+myLove = "I love only JS";
+console.log("My Love is :",myLove);
+
+console.log("--------------------------------------------");
+var firstName = "Bindiya";
+console.log("First Name :", firstName);
+var lastName = "Shetty";
+console.log("Last Name :",lastName);
+var age = 23;
+console.log("Age :", age);
+var add = "Pune";
+console.log("Address :", add);
+var pinCode = "411061";
+console.log("Pincode :", pinCode);
+var gender = "Female";
+console.log("Gender :", gender);
+var isMarried = "No";
+console.log("Is Married :", isMarried);
+console.log("---------------------------------------------------------");
+
