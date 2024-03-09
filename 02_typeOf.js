425537b55cf112e2c2efd53f5c262f45f8091fbd
BindiyaShetty18 <shettybindiya18@gmail.com>
2024-03-09 22:54:42 +0530

    Using data types - typeof
    Using data types - typeof

---
+++ 02_typeOf.js
@@ -0,0 +1,43 @@
+console.log("==== Step 1 ====");
+console.log("----- Before Initialization -----");
+
+var bankName;
+console.log("Bank Name :", bankName);
+typeof bankName;
+console.log("Data Type : :", bankName);
+console.log("----- After Initialization -----");
+bankName = "SBI Bank";
+console.log("Bank Name :", bankName);
+var typeOfBank = typeof bankName;
+console.log("Data Type : :", typeOfBank);
+
+console.log("==== Step 2 ====");
+var marks = "90%";
+console.log("Marks :",marks);
+var typeOfMarks = typeof marks;
+console.log("Data Type :",typeOfMarks);
+
+console.log("==== Step 3 ====");
+console.log("----- Before Initialization -----");
+var isWorking;
+console.log("Is Working :",isWorking);
+var typeOfMarriage = typeof isWorking;
+console.log("Data Type :",isWorking);
+console.log("----- After Initialization -----");
+isWorking = true;
+console.log("Is Working :",isWorking);
+var typeOfMarriage = typeof isWorking;
+console.log("Data Type :",typeOfMarriage);
+
+console.log("==== Step 4 ====");
+console.log("----- Before Update -----");
+var totalCount = "Hundred and Seven;"
+console.log("Value :",totalCount);
+var typeOfValue = typeof totalCount;
+console.log("Data Type :",typeOfValue);
+console.log("----- After Update -----");
+totalCount = 107;
+console.log("Value :",totalCount);
+var typeOfValue = typeof totalCount;
+console.log("Data Type :",typeOfValue);
+
