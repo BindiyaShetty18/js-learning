cad87cc4b41be6953f01f7cf53e6bf7d81e5d2e5
BindiyaShetty18 <shettybindiya18@gmail.com>
2024-03-13 06:26:45 +0530

    String Template Assignment
    String Template Assignment

---
+++ 03_stringBasicAssign01.js
@@ -0,0 +1,13 @@
+function stringBasics(){
+    var myDreamCompany = "Oracle";
+    console.log(`My Dream company is ${myDreamCompany}`);
+    var hobby1 = "Gardening";
+    var hobby2 = "Reading";
+    var hobby3 = "Listening Music";
+    console.log(`My hobbies are ${hobby1}, ${hobby2}, ${hobby3}`);
+    console.log(`My first hobby is ${hobby1} and its length is ${hobby1.length}`);
+    console.log(`My first hobby is ${hobby2} and its length is ${hobby2.length}`);
+    console.log(`My first hobby is ${hobby3} and its length is ${hobby3.length}`);
+    
+}
+stringBasics();
---
+++ 04_stringTemplate.js
@@ -0,0 +1,7 @@
+console.log(`I am a "Software" Developer`);
+
+var city = "Pune";
+var firstName = "Bindiya";
+var totalExperience = 11;
+
+console.log(`My name is : ${firstName} and my city is : ${city} and total experience : ${totalExperience}`);
